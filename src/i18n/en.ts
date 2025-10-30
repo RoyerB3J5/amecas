eb66d40e75fad2e3d3959b7eee7bae01269263ba
Royer Bonifacio <royer.boni13@gmail.com>
2025-10-30 12:04:08 -0500

    add:home page

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -1,34 +1,82 @@
-export default{
-  header:{
-    button:"Learn More",
-    nav:[
+export default {
+  header: {
+    button: 'Learn More',
+    nav: [
       {
-        label:"Home",
-        href:"/"
+        label: 'Home',
+        href: '/',
       },
       {
-        label:"Menu",
-        href:"/menu"
+        label: 'Menu',
+        href: '/menu',
       },
       {
-        label:"Catering",
-        href:"/catering"
+        label: 'Catering',
+        href: '/catering',
       },
       {
-        label:"Order",
-        href:"#"
+        label: 'Order',
+        href: '#',
       },
       {
-        label:"Info",
-        href:"/info"
+        label: 'Info',
+        href: '/info',
       },
-    ]
+    ],
   },
-  main:{
-    hero:{
-      text:"Our Services",
-      title: "<span class='text-accent-1'>select</span> <br /> what you <br /> need",
-      button:"LEARN MORE"
-    }
-  }
-}
+  main: {
+    hero: {
+      text: 'Our Services',
+      title:
+        "<span class='text-accent-1'>select</span> <br /> what you <br /> need",
+      button: 'LEARN MORE',
+      slides: [
+        {
+          image: 'bg-accent-2',
+          text: 'Our Services',
+          title:
+            "<span class='text-accent-1'>select</span> <br /> what you <br /> need",
+          button: 'LEARN MORE',
+        },
+        {
+          image: 'bg-accent-3',
+          text: 'Our Services',
+          title:
+            "<span class='text-accent-2'>select</span> <br /> what you <br /> need",
+          button: 'LEARN MORE',
+        },
+        {
+          image: 'bg-accent-1',
+          text: 'Our Services',
+          title:
+            "<span class='text-accent-3'>select</span> <br /> what you <br /> need",
+          button: 'LEARN MORE',
+        },
+      ],
+    },
+    service: {
+      text: 'Our Services',
+      title: 'select what you need',
+      list: [
+        {
+          label: 'Order',
+          button: 'see more',
+          image: 'order',
+          color: 'bg-accent-1',
+        },
+        {
+          label: 'Catering',
+          button: 'see more',
+          image: 'catering',
+          color: 'bg-accent-2',
+        },
+        {
+          label: 'Menu',
+          button: 'see more',
+          image: 'menu-2',
+          color: 'bg-accent-3',
+        },
+      ],
+    },
+  },
+};
--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -1,34 +1,82 @@
-export default{
-  header:{
-    button:"Conocer Más",
-    nav:[
+export default {
+  header: {
+    button: 'Conocer Más',
+    nav: [
       {
-        label:"Inicio",
-        href:"/"
+        label: 'Inicio',
+        href: '/',
       },
       {
-        label:"Menú",
-        href:"/menu"
+        label: 'Menú',
+        href: '/menu',
       },
       {
-        label:"Catering",
-        href:"/catering"
+        label: 'Catering',
+        href: '/catering',
       },
       {
-        label:"Pedido",
-        href:"#"
+        label: 'Pedido',
+        href: '#',
       },
       {
-        label:"Info",
-        href:"/info"
+        label: 'Info',
+        href: '/info',
       },
-    ]
+    ],
   },
-  main:{
-    hero:{
-      text:"Nuestros Servicios",
-      title: "<span class='text-accent-1'>selecciona</span> <br /> lo que<br /> necesites",
-      button:"CONOCER MÁS"
+  main: {
+    hero: {
+      text: 'Nuestros Servicios',
+      title:
+        "<span class='text-accent-1'>selecciona</span> <br /> lo que<br /> necesites",
+      button: 'CONOCER MÁS',
+      slides: [
+        {
+          image: "bg-accent-2",
+          text: 'Nuestros Servicios',
+          title:
+            "<span class='text-accent-1'>selecciona</span> <br /> lo que<br /> necesites",
+          button: 'CONOCER MÁS',
+        },
+        {
+          image: "bg-accent-3",
+          text: 'Nuestros Servicios',
+          title:
+            "<span class='text-accent-2'>selecciona</span> <br /> lo que<br /> necesites",
+          button: 'CONOCER MÁS',
+        },
+        {
+          image: "bg-accent-1",
+          text: 'Nuestros Servicios',
+          title:
+            "<span class='text-accent-3'>selecciona</span> <br /> lo que<br /> necesites",
+          button: 'CONOCER MÁS',
+        },
+      ],
+    },
+    service:{
+      text: 'Nuestros Servicios',
+      title:"selecciona lo que necesites",
+      list:[
+        {
+          label:"Orden",
+          button:"ver más",
+          image:"order",
+          color:"bg-accent-1"
+        },
+        {
+          label:"Catering",
+          button:"ver más",
+          image:"catering",
+          color:"bg-accent-2"
+        },
+        {
+          label:"Menú",
+          button:"ver más",
+          image:"menu-2",
+          color:"bg-accent-3"
+        }
+      ]
     }
-  }
-}
+  },
+};
