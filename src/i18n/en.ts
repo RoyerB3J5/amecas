6fe1fbd6e017c9c89306849de8a455aca8b7fddb
Royer Bonifacio <royer.boni13@gmail.com>
2025-10-30 00:27:21 -0500

    add:header

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -1,3 +1,34 @@
 export default{
-  
+  header:{
+    button:"Learn More",
+    nav:[
+      {
+        label:"Home",
+        href:"/"
+      },
+      {
+        label:"Menu",
+        href:"/menu"
+      },
+      {
+        label:"Catering",
+        href:"/catering"
+      },
+      {
+        label:"Order",
+        href:"#"
+      },
+      {
+        label:"Info",
+        href:"/info"
+      },
+    ]
+  },
+  main:{
+    hero:{
+      text:"Our Services",
+      title: "<span class='text-accent-1'>select</span> <br /> what you <br /> need",
+      button:"LEARN MORE"
+    }
+  }
 }
--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -1,3 +1,34 @@
 export default{
-  
+  header:{
+    button:"Conocer Más",
+    nav:[
+      {
+        label:"Inicio",
+        href:"/"
+      },
+      {
+        label:"Menú",
+        href:"/menu"
+      },
+      {
+        label:"Catering",
+        href:"/catering"
+      },
+      {
+        label:"Pedido",
+        href:"#"
+      },
+      {
+        label:"Info",
+        href:"/info"
+      },
+    ]
+  },
+  main:{
+    hero:{
+      text:"Nuestros Servicios",
+      title: "<span class='text-accent-1'>selecciona</span> <br /> lo que<br /> necesites",
+      button:"CONOCER MÁS"
+    }
+  }
 }
